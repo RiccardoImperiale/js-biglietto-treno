547113cc1bb0147139dbe1bf247e8a6bf7f9da5c
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-02-19 14:19:59 +0100

    add prompts to get user trip info

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+
+// Ask the user the kilometers he want to travel and his age
+const tripKm = Number(prompt('How many kilometers do you like to travel?'));
+const userAge = Number(prompt('How old are you'));
+
+console.log(tripKm, userAge);
