1f2db2232c387024fbfcfaea353eeca7067aa43d
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-02-19 15:03:58 +0100

    minor fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ const kidsDiscount = 20;
 const eldersDiscount = 40;
 
 // get kilometers and age of the user
-const tripKm = Number(prompt('How many kilometers do you like to travel?'));
+const tripKm = Number(prompt('How many kilometers would you like to travel?'));
 const userAge = Number(prompt('How old are you'));
 
 // multiply user km by 0.21 € to get the full price
